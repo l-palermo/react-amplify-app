d46afef792fffc6b91dda6ce2ff7645f3a7de00c
l-palermo <l-palermo@hotmail.it>
2020-05-25 22:54:14 +0100

    add webpack to compile JavaScript modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 module.exports = {
   mode: "development",
+  // entry point to load all the dependencies/modules included in the app
   entry: "./src/index.js",
+  // where the bundle will be stored with props
   output: {
     filename: "bundle.js",
-    path: __dirname + "dist",
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     // tells webpack where to take the content from
