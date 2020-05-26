b018df73afefad1bca4cca681522c9bb928189ec
l-palermo <l-palermo@hotmail.it>
2020-05-26 15:29:38 +0100

    add amplify to react app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 // seems I have no need for react-hot-loader
 
-import style from './style.css';
+import style from "./style.css";
 
-const App = () => <h1>Hello world 2</h1>;
+const App = () => (
+  <React.Fragment>
+    <h1>Hello world 2</h1><AmplifySignOut />
+  </React.Fragment>
+);
 
-export default App;
+export default withAuthenticator(App, {
+  usernameAlias: "email",
+});
