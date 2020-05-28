c8b825301b813d29685ef45cdd3401424ebefba6
l-palermo <l-palermo@hotmail.it>
2020-05-28 12:20:50 +0100

    add env to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,61 @@
 const path = require("path");
 const webpack = require("webpack");
-// const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  // entry point to load all the dependencies/modules included in the app
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      // js, jsx rules
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          rootMode: "upward",
-          presets: ["@babel/env"],
+module.exports = function (webpackEnv) {
+  const isEnvProduction = webpackEnv === 'production';
+  return {
+    mode: isEnvProduction ? 'production' : 'development',
+    // entry point to load all the dependencies/modules included in the app
+    entry: "./src/index.js",
+    module: {
+      rules: [
+        // js, jsx rules
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: {
+            rootMode: "upward",
+            presets: ["@babel/env"],
+          },
         },
-      },
-      // css rule
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+        // css rule
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      // console error were due to .mjs extension missing
+      extensions: ["*", ".mjs", ".js", ".jsx"],
+    },
+    // where the bundle will be stored
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      // tells webpack where to take the content from
+      contentBase: "./dist",
+      // needs to be specified ...?
+      port: 8000,
+      // compress the bundle
+      compress: true,
+      // allows the hot module replacement
+      hotOnly: true,
+    },
+    // allows no page refresh
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  resolve: {
-    // console error were due to .mjs extension missing
-    extensions: ["*", ".mjs", ".js", ".jsx"],
-  },
-  // where the bundle will be stored
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    // tells webpack where to take the content from
-    contentBase: "./dist",
-    // needs to be specified ...?
-    port: 8000,
-    // compress the bundle
-    compress: true,
-    // allows the hot module replacement
-    hotOnly: true,
-  },
-  // allows no page refresh
-  plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    // new CopyWebpackPlugin({ patterns: ["./dist/index.html"] }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  // enables source maps
-  devtool: "source-map",
+    // enables source maps
+    devtool: webpackEnv === 'development' ? "inline-source-map" : "source-map",
+  };
 };
