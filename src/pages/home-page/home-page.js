484a899db0847373f0fc6e30275d4ff7dada22c9
l-palermo <l-palermo@hotmail.it>
2020-06-03 13:13:42 +0100

    add gfycat

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './home-page.module.css';
 import Container from '../../components/container';
+import fetchTrendingGifs from '../../helpers/fetch-home-content/fetch-home-content';
 
 const HomePage = () => {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    fetchTrendingGifs().then((data) => setGifs(data.gfycats));
+  });
+
   return (
     <div data-qa="home-page">
       <Container>
         <div className={styles.div0}>
-          <div className={styles.div1} />
-          <div className={styles.div2} />
-          <div className={styles.div3} />
-          <div className={styles.div4} />
-          <div className={styles.div5} />
+          {gifs.map((gif) => {
+            const { gifUrl, gfyName, gfyId } = gif;
+            return (
+              <div key={gfyId} className={styles.div1}>
+                <img className={styles.image} src={gifUrl} alt={gfyName} />
+              </div>
+            );
+          })}
         </div>
       </Container>
     </div>
